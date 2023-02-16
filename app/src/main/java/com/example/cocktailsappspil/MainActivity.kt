82ababd5d7f566444a1fdcf71873f8e0b5bd6d25
Sara Iza <sasita.iza@gmail.com>
2023-02-15 23:57:19 -0500

    Created layouts and fragments

--- app/src/main/java/com/example/cocktailsappspil/MainActivity.kt
+++ app/src/main/java/com/example/cocktailsappspil/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.cocktailsappspil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cocktailsappspil.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager
+            .findFragmentById(R.id.frag_container) as NavHostFragment
+
+        setupActionBarWithNavController(navHost.navController)
+        binding.bottomMenu.setupWithNavController(navHost.navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.frag_container).navigateUp()
+    }
+
 }
